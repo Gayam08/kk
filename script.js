2b0baf3aac38340f87bbfb4bd4b53e780e5468b4
Gaya MOULAHCENE <153306534+Gayam08@users.noreply.github.com>
2024-07-11 19:28:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let n1 = document.querySelector("#nb1");
+let n2 = document.querySelector("#nb2");
+let op = document.querySelector("#op");
+let calcbtn = document.querySelector("#calcbtn");
+let ores = document.querySelector("#res");
+let result = document.querySelector("#result");
+
+calcbtn.onclick = function() {
+    let res;
+    let opr = op.value;
+    switch (opr) {
+        case "+":
+            res = Number(n1.value) + Number(n2.value);
+            break;
+        case "-":
+            res = Number(n1.value) - Number(n2.value);
+            break;
+        case "*":
+            res = Number(n1.value) * Number(n2.value);
+            break;
+        case "/":
+            res = Number(n1.value) / Number(n2.value);
+            break;
+        case "%":
+            res = Number(n1.value) % Number(n2.value);
+            break;
+    }
+    ores.innerHTML = "a npnor ayaghyoul";
+    result.style.visibility = "visible";
+};
